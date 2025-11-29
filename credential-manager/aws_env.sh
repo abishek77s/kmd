ef56f4bf13a1971792dbde62331d5bed66bae6f4
abishek77s <abishek22004@gmail.com>
2025-11-29 21:56:51 +0530

    removed others except cli

--- credential-manager/aws_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export AWS_ACCESS_KEY_ID="919728787723"
-export AWS_SECRET_ACCESS_KEY="2uyhikjn,dxab768276371"
-export AWS_DEFAULT_REGION="us-east1"
-echo "AWS credentials exported to environment"
-echo "Run: source aws_env.sh"
-echo "Or copy-paste the export commands above"
